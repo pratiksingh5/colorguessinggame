a7c18ccb4e9d60a470bcb2bb1b15574e834d46e2
Pratik Singh <pratikrajaryan@gmail.com>
2023-11-04 18:58:53 +0530

    added timer

---
+++ function.js
@@ -0,0 +1,19 @@
+// function getName(){
+//     // console.log('Lenovo')
+//     // let sum = 1 + 4 +2
+//     let firstName = "John";
+//     let lastName = "Doe"
+//     let fullName = firstName + " " + lastName
+//     return firstName
+// }
+
+// let test = getName()
+// console.log(test)
+
+function getRandomNumber(){
+    let random = Math.random() * 10;
+    let leftSideWalaNumber = Math.floor(random)
+    console.log(leftSideWalaNumber)
+}
+
+getRandomNumber()
--- script.js
+++ script.js
@@ -27,17 +27,58 @@ light.addEventListener("click", function(){
 
 let circle = document.querySelector('.circle');
 let canvas = document.querySelector('.canvas');
+let guessNumber = document.querySelector('#guessNumber');
+
+guessNumber.innerHTML = getRandomNumber();
 
 function makeCircles(){
-    console.log("Circle creation starts");
-
-
-    for(let i = 0; i < 36 ; i++){
-        // canvas.innerHTML = `<div class="circle">5</div>`;
-        canvas.innerHTML = canvas.innerHTML +`<div class="circle">5</div>`
-        console.log('circle created', i)
-    }
-
+  for(let i =0; i< 44; i++){
+    let cloneCircle = circle.cloneNode(true);
+    let random = getRandomNumber();
+    cloneCircle.textContent = random;
+    canvas.appendChild(cloneCircle)
+  }
 }
 
-makeCircles()
+makeCircles();
+
+function getRandomNumber(){
+  let random = Math.random() * 10;
+  random = Math.floor(random);
+  return random;
+}
+
+
+
+// function makeCircles(){
+//     console.log("Circle creation starts");
+
+
+//     for(let i = 0; i < 36 ; i++){
+//         // canvas.innerHTML = `<div class="circle">5</div>`;
+//         canvas.innerHTML = canvas.innerHTML +`<div class="circle">5</div>`
+//         console.log('circle created', i)
+//     }
+
+// }
+
+// makeCircles()
+
+
+
+
+// TIMER KE CODE ME 
+// setInterval(function, time)
+
+
+let timeLeft = 30;
+let clock = document.querySelector('#clock')
+
+function decreaseTime(){
+   if(timeLeft > 0){
+    timeLeft = timeLeft - 1;
+    clock.innerHTML = timeLeft;
+   }
+}
+
+setInterval(decreaseTime, 1000)
